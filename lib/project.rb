7d75fdb1a99d441a0fdf72d043ed8d0cd791c8a3
sun5hine <af5kh@yahoo.com>
2018-11-03 22:35:47 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -4,9 +4,13 @@ class Project
   
   @@backers =[]
   
-  def initialize()
-    
+  def initialize(project_name)
+    @project_name  = project_name
     @backers << self 
   end
+  
+  def self.backers
+    @@backers
+  end 
 
 end 
